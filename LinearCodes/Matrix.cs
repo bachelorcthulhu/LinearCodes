aa7b5b43c40f1a8c5c33ce905995a4605d597166
bachelor.vladislav.fedorov@gmail.com <bachelor.vladislav.fedorov@gmail.com>
2023-04-18 22:06:27 +0700

    Add project files.

---
+++ LinearCodes/Matrix.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearCodes
+
+{
+    public class Matrix
+    {
+        /// <summary>
+        /// N - количество строк в матрице
+        /// </summary>
+        public int Rows { get; private set; }
+        /// <summary>
+        /// K - количество столбцов в матрице
+        /// </summary>
+        public int Columns { get; private set; }
+
+        public char[,] Data { get; set; }
+
+
+
+        public Matrix(int _rows, int _columns)
+        {
+            Rows= _rows;
+            Columns= _columns;
+            Data = new char[Rows, Columns];
+        }
+
+        public string ConvertToBinary(int _number, int _N)
+        {
+            string binaryNumber = Convert.ToString(_number, 2);
+            int distance;
+
+            if (binaryNumber.Length == _N) 
+                return binaryNumber;
+
+            distance = _N - binaryNumber.Length;
+            return binaryNumber.PadLeft(binaryNumber.Length + distance, '0');
+        }
+
+        public bool isPowerOfTwo(int n)
+        {
+            return (int)(Math.Ceiling((Math.Log(n) / Math.Log(2))))
+                  == (int)(Math.Floor(((Math.Log(n) / Math.Log(2)))));
+        }
+
+        public void MakeSimpleHMatrix()
+        {
+            int number = 3;
+            int powerOfTwo = 0;
+
+            string binaryNumber;
+
+            for (int i = Columns - 1; i >= Rows; i--)
+            {
+                binaryNumber = ConvertToBinary((int)Math.Pow(2, powerOfTwo), Rows);
+                for (int j = 0; j < Rows; j++)
+                {
+                    Data[j, i] = binaryNumber[j];
+                }
+
+                powerOfTwo++;
+            }
+
+
+            for(int i = 0; i < Columns - Rows; i++)
+            {
+                binaryNumber = ConvertToBinary(number, Rows);
+
+                for (int j = 0; j < Rows; j++)
+                {
+                    Data[j, i] = binaryNumber[j];
+                }
+
+                number++;
+                if (isPowerOfTwo(number))
+                {
+                    number++;
+                }
+            }
+        }
+
+        public void MakeSimpleGMatrix(Matrix _hMatrix) 
+        {
+            int powerOfTwo = 0;
+
+            string binaryNumber;
+
+            int hMatrixRow = 0;
+
+            int columnToStart = Columns - _hMatrix.Rows - 1;
+
+            for (int i = 0; i <= columnToStart; i++)
+            {
+                binaryNumber = ConvertToBinary((int)Math.Pow(2, powerOfTwo), Rows);
+                for (int j = 0; j < Rows; j++) //_hMatrix.Columns - _hMatrix.Rows
+                {
+                    Data[j, i] = binaryNumber[Rows-1-j];
+                    //Console.Write("{0} ", Data[j, i]);
+                }
+                //Console.WriteLine();
+
+                powerOfTwo++;
+            }
+
+            for (int i = columnToStart+1; i < Columns; i++) // for (int i = (_hMatrix.Rows + 1); i < Columns; i++)
+            {
+                for (int j = 0; j < Rows; j++)
+                {
+                    Data[j,i] = _hMatrix.Data[hMatrixRow, j];
+                    //Console.Write("{0} ", Data[j, i]);
+                }
+                hMatrixRow++;
+               // Console.WriteLine();
+            }            
+        }
+    }
+
+    public class MatrixG : Matrix
+    {
+        
+    }
+}
---
+++ LinearCodes/Program.cs
@@ -0,0 +1,34 @@
+﻿using LinearCodes;
+
+int rows = 3; //3
+int columns = 7; //7
+
+Matrix matrixH = new Matrix(rows, columns);
+matrixH.MakeSimpleHMatrix();
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write("{0} ", matrixH.Data[i, j]);
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+rows = 4; //4
+columns = 7; //7
+
+Matrix matrixG = new Matrix(rows, columns);
+matrixG.MakeSimpleGMatrix(matrixH);
+
+
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write("{0} ", matrixG.Data[i, j]);
+    }
+    Console.WriteLine();
+}
